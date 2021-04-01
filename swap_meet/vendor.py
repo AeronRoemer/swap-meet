33b57c12419d065c8a48b062e383611fcf44e97a
AerR <57688724+AeronRoemer@users.noreply.github.com>
2021-04-01 11:22:22 -0600

    Vendor created and passing wave 1

---
+++ swap_meet/vendor.py
@@ -0,0 +1,13 @@
+class Vendor:
+    def __init__(self, inventory=[]):
+        self.inventory = inventory
+    
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+    
+    def remove(self, item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+            return item
+        return False
