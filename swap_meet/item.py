9385047ad07df9da96578d3d9f14b6fcc4143231
AerR <57688724+AeronRoemer@users.noreply.github.com>
2021-04-01 20:59:50 -0600

    Added String functionality to Item and swapping to vendors

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,4 +1,7 @@
 class Item:
-    
+
     def __init__(self, category=""):
         self.category = category
+
+    def __str__(self):
+        return "Hello World!"
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -2,20 +2,33 @@ class Vendor:
     def __init__(self, inventory=None):
         self.inventory = inventory
     
+    # add an item to the Vendor's inventory
     def add(self, item):
         self.inventory.append(item)
         print(self.inventory)
         return item
-    
+
+    # remove an item from the Vendor's inventory
     def remove(self, item):
         if item in self.inventory:
             self.inventory.remove(item)
             return item
         return False
 
+    # matches items in the vendor's inventory with a sought param
     def get_by_category(self, sought_category):
         matching_category = []
         for item in self.inventory:
             if item.category == sought_category:
                 matching_category.append(item)
         return matching_category
+    
+    # swaps an item with another vendor
+    def swap_items(self, recipient, self_item, other_persons_item):
+        if self_item in self.inventory and other_persons_item in recipient.inventory:
+            recipient.inventory.append(self_item)
+            self.inventory.remove(self_item)
+            recipient.inventory.remove(other_persons_item)
+            self.inventory.append(other_persons_item)
+            return True   
+        return False
