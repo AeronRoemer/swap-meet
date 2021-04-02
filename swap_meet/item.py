d337a32e6a59bbbb5f363583654bd23bcfce0f9d
AerR <57688724+AeronRoemer@users.noreply.github.com>
2021-04-01 20:23:11 -0600

    Item class added - Test Wave 2 passing

---
+++ swap_meet/item.py
@@ -0,0 +1,4 @@
+class Item:
+    
+    def __init__(self, category=""):
+        self.category = category
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,9 +1,10 @@
 class Vendor:
-    def __init__(self, inventory=[]):
+    def __init__(self, inventory=None):
         self.inventory = inventory
     
     def add(self, item):
         self.inventory.append(item)
+        print(self.inventory)
         return item
     
     def remove(self, item):
@@ -11,3 +12,10 @@ class Vendor:
             self.inventory.remove(item)
             return item
         return False
+
+    def get_by_category(self, sought_category):
+        matching_category = []
+        for item in self.inventory:
+            if item.category == sought_category:
+                matching_category.append(item)
+        return matching_category
